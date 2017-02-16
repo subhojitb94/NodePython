96e38573011ef348261da34f517375ce419d07d1
Sarav Chatterjee <saurav.chatterjee91@gmail.com>
2017-02-17 00:03:18 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+//var dataAccessLayer=require("./DAL.js");
+var app  = express();
+
+function CRUD(){
+    var self = this;
+    self.configureExpress();
+};
+
+
+CRUD.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+
+      router.post("/users",function(req,res){
+        console.log(req.body.uid);
+        res.send("Succsess!!")  
+      }); 
+
+       router.get("/",function(req,res){
+        console.log("Welome to our api powered by node js");  
+        res.send("Succsess!!")
+      }); 
+      //var crud_router = new dataAccessLayer(router,connection,md5);
+      self.startServer();
+}
+
+
+CRUD.prototype.startServer = function() {
+      app.listen(3000,function(){
+          console.log("Server successfully started at Port 3000.");
+      });
+}
+
+CRUD.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL \n" + err);
+    process.exit(1);
+}
+
+new CRUD();
